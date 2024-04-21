b9578ab17523a31e9475fe76833e0074f558f05d
mahengjie <mahengjie@star-net.cn>
2024-04-21 17:17:17 +0800

    BUGFIX 修复部署无阿拉伯语错误

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -142,7 +142,7 @@ function Screen() {
           config.tightBorder && !isMobileScreen
             ? styles["tight-container"]
             : styles.container
-        } ${getLang() === "ar" ? styles["rtl-screen"] : ""}`
+        }`
       }
     >
       {isAuth ? (
